c38354958f7e57c2caecd5913f1fba5fe1d64736
Tanveerhusyn <tanveerhussain465@gmail.com>
2022-05-17 01:13:24 +0500

    make it better

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,16 +1,18 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 // create here mysql connection
 //mysql://b021387c5e5af6:a85c63c1@us-cdbr-east-05.cleardb.net/heroku_44a14e02619144e?reconnect=true
 
+// HOST: 'localhost',
+//     USER: 'root',
+//     PASSWORD: '',
+//     DB: 'ecommerce_store',
 
-module.exports ={
-    HOST: 'us-cdbr-east-05.cleardb.net',
-    USER: 'b021387c5e5af6',
-    PASSWORD: 'a85c63c1',
-    DB: 'heroku_44a14e02619144e',
-    
-
+module.exports = {
+  HOST: "us-cdbr-east-05.cleardb.net",
+  USER: "b021387c5e5af6",
+  PASSWORD: "a85c63c1",
+  DB: "heroku_44a14e02619144e",
 };
 
 // dbConn.connect(function(error){
--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const path = require('path')
 // create express app
 const app = express();
 var cors = require('cors');
 app.use(cors({origin: true, credentials: true}));
 
 // setup the server port
- 
 
+
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname,'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 // parse request data content type application/x-www-form-rulencoded
 app.use(bodyParser.urlencoded({extended: false}));
 
